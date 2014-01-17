858fae4fd8d3e973108cc493511f4564ef1eb279
samuel-stromswold <samstromsw@gmail.com>
2014-01-16 20:39:24 -0500

    Update match_view.php
    added new fields (fouls,tech fouls,problems,win/loss)

--- match_view.php
+++ match_view.php
@@ -1,29 +1,35 @@
 <?php
 
 $team = 0;
-$heads=array("<td>autonomous</td>","<td>tosses over bar</td>","<td>catches from bar</td>","<td>passes</td>","<td>recieved</td>","<td>high goals</td>","<td>low goals</td>","<td>defense estimate</td>","<td>balls killed</td>","<td>notes</td>","<td>tags</td>");//stores the column header names
+$heads=array("<td>autonomous</td>","<td>tosses over bar</td>","<td>catches from bar</td>","<td>passes</td>","<td>recieved</td>","<td>high goals</td>","<td>low goals</td>","<td>defense estimate</td>","<td>fouls</td>","<td>technical fouls</td>","<td>problems</td>","<td>won/lost</td>","<td>notes</td>","<td>tags</td>");//stores the column header names
+
+$team = "*";
+$auton = $barToss = $barCatch = $passes = $recieve = $goalHigh =$goalLow = $defense = $fouls = $tech_fouls = $tech_problems = $won = $notes = $tags = True;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	// define variables and set to default values
-	$auton = $barToss = $barCatch = $passes = $recieve = $goalHigh = $goalLow = $defense = $killed = $notes = $tags =  False;
+	///$barToss = $barCatch = $passes = $recieve = $goalHigh = $goalLow = $defense = $killed = $notes = $tags =  False;
 	
 	//This entire thing just assigns the form data to these variables
-	$team = $_POST["team"];
-	$auton = (strcmp($_POST["shot_auton"],"high")==0);
-	$barToss = (strcmp($_POST["bar_toss"],"high")==0);
-	$barCatch = (strcmp($_POST["bar_catch"],"high")==0);
-	$passes = (strcmp($_POST["passes"],"high")==0);
-	$recieve = (strcmp($_POST["recieved"],"high")==0);
-	$goalHigh = (strcmp($_POST["goal_high"],"high")==0);
-	$goalLow = (strcmp($_POST["goal_low"],"high")==0);
-	$defense = (strcmp($_POST["defense"],"high")==0);
-	$killed = (strcmp($_POST["killed"],"high")==0);
-	$notes = (strcmp($_POST["notes"],"high")==0);
-	$tags = (strcmp($_POST["tags"],"high")==0);
+	$auton = (strcmp($_POST["auton"],"shot_auton")==0);
+	$barToss = (strcmp($_POST["bar_toss"],"bar_toss")==0);
+	$barCatch = (strcmp($_POST["bar_catch"],"bar_catch")==0);
+	$passes = (strcmp($_POST["passes"],"passes")==0);
+	$recieve = (strcmp($_POST["recieved"],"recieved")==0);
+	$goalHigh = (strcmp($_POST["goal_high"],"goal_high")==0);
+	$goalLow = (strcmp($_POST["goal_low"],"goal_low")==0);
+	$defense = (strcmp($_POST["defense"],"dfense")==0);
+	$fouls = (strcmp($_POST["fouls"],"fouls")==0);
+	$tech_fouls = (strcmp($_POST["tech_fouls"],"tech_fouls")==0);
+	$tech_problems = (strcmp($_POST["tech_problems"],"tech_problems")==0);
+	$won = (strcmp($_POST["won"],"won")==0);
+	$notes = (strcmp($_POST["notes"],"notes")==0);
+	$tags = (strcmp($_POST["tags"],"tags")==0);
+	
+	//stores the boolean values from the form responses
+	$cols=array($auton,$barToss,$barCatch,$passes,$recieve,$goalHigh,$goalLow,$defense,$fouls,$tech_fouls,$tech_problems,$won,$notes,$tags);
 }
 
-//stores the boolean values from the form responses
-$cols=array($auton,$barToss,$barCatch,$passes,$recieve,$goalHigh,$goalLow,$defense,$killed,$notes,$tags);
 
 $con=mysqli_connect("localhost","root","","scouting_database");
 
@@ -61,7 +67,7 @@ while($row = mysqli_fetch_array($result)){
 	
 	
 	
-	echo "<tr> <td>".$row['match']."</td> <td>".$row['team']."</td> <td>".$auton."</td> <td>".$row['bar_toss']."</td> <td>".$row['bar_catch']."</td> <td>".$row['passes']."</td> <td>".$row['recieved']."</td> <td>".$row['goal_high']."</td> <td>".$row['goal_low']."</td> <td>".$row['defense']."</td> <td>".$row['killed']."</td> <td>".$row['notes']."</td> <td>".$row['tags']."</td> </tr>";
+	echo "<tr> <td>".$row['match']."</td> <td>".$row['team']."</td> <td>".$auton."</td> <td>".$row['bar_toss']."</td> <td>".$row['bar_catch']."</td> <td>".$row['passes']."</td> <td>".$row['recieved']."</td> <td>".$row['goal_high']."</td> <td>".$row['goal_low']."</td> <td>".$row['defense']."</td> <td>".$row['fouls']."</td> <td>".$row['tech_fouls']."</td> <td>".$row['tech_problems']."</td> <td>".$row['win/lose']."</td> <td>".$row['notes']."</td> <td>".$row['tags']."</td> </tr>";
 }
 echo "</table>"
 
