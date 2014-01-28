a407b06500289c7d80a1ff1c94d261a7fab56fc4
samuel-stromswold <samstromsw@gmail.com>
2014-01-28 18:47:34 -0500

    tick(),setTask(String s)
    Added implementation for the robot->tick() method and robot->setTask() method, setTask() now accepts strings "pass","shoot","defend", and the "" for rest
    TODO: when we combine all the files be sure to put the constant TICK_TIME back in, and come up with a good value

--- Robot.php
+++ Robot.php
@@ -1,4 +1,6 @@
 <?php
+define("TICK_TIME",'5.0');//the time deducted with each tick
+
 $con=mysqli_connect("localhost","root","","scouting_database");
 $result_match=mysqli_query($con,"SELECT * FROM 'match_data'");
 $result_team=mysqli_query($con,"SELECT * FROM 'robot_data'")
@@ -13,24 +15,22 @@ class Robot{
 	private $team = -1;
 	private $avgDefense = -1;
 	private $shoot = -1;//0=can't shoot, 1=can shoot low, 2=can shoot high, 3=can shoot both
-	private $passSpeed = -1;
 	private $autonShoot =-1;//0=can't shoot, 6=shoots low, 15=shoots high, 11=shoots hot low, 20=shoots hot high
-	private $scoreProb = -1.0
+	private $passSpeed = -1;//for now we are using the same variable for passing and shooting, partly to make scouting easier
+	private $scoreProb = -1.0;
 	private $chatchProb = -1.0;
 	private $passOver = False;
 	private $catchOver = False;
 	private $autonDrive = False;
 	
 	private $task="";
-	private $timeLEftTask=-1;
+	private $taskTimeLeft=-1;//-1 is invalid, continue until another task is set
 	private $hasBall = True;//this is declared true as per the rules
 	
 	public function __construct(){
 		
 	}
-	
 
-	
 	public function pass(){//attempts to pass and returns whether the attempt was successful
 		if(rand(0,1)<=$scoreProb){ //TODO?: use a better random number generator?
 			if($hasBall){
@@ -41,7 +41,31 @@ class Robot{
 		return False;
 	}
 	
+	//sets the new task for a robot to work on
+	/*-ACCEPTS STRINGS- valid strings are "pass","shoot", "defend", 
+	and an empty string for resting*/
+	public function setTask($newTask){
+		$task=$newTask;
+		if(strcmp($newTask,"pass")==0 || strcmp($newTask,"shoot"))//for now I am using the same variable for passing and shooting
+			$taskTimeLeft=$passSpeed;
+		else if(strcmp($newTask,"defend")==0)
+			$taskTimeLeft=-1;
+	}
 	
+	public function tick(){
+		if($taskTimeLeft!=-1){
+			$taskTimeLeft-=TICK_TIME;
+		}
+		if($taskTimeLeft<=0){
+			if(strcmp($task,"pass")==0)
+				return pass();
+			if(strcmp($task,"shoot")==0)
+				return pass();
+			if(strcmp($task,"defend")==0)
+				return floor(rand(0,1)*defense*TICK_TIME/140);
+		}
+			
+	}
 	
 	{//getters
 	public function getTeam(){
