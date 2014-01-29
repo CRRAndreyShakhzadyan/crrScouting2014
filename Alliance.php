1c147fd099e4d76bcc86143af19c640c65ffdf1d
samuel-stromswold <samstromsw@gmail.com>
2014-01-28 19:21:47 -0500

    Update Alliance.php
    Added the tick() method for the alliance class and moved over a few variables from match

--- Alliance.php
+++ Alliance.php
@@ -6,6 +6,10 @@ class Alliance{
 	private $robotB=new Robot();
 	private $robotC=new Robot();
 	
+	private $ballHeat=0;
+	private $score=0;
+	private $passedOver=false;
+	
 	private $robotTeam = array($robotA,$robotB,$robotC);
 	
 	public function __construct(Robot $a, Robot $b, Robot $c){
@@ -22,6 +26,27 @@ class Alliance{
 		return $robotTeam;
 	}
 	
-
+	//passes the ball to a random robot params: 
+	//$not: what robot just passed the ball and therefore can't recieve
+	/*-IMPORTANT- This method doesn't do anything to remove the ball from
+	one robot, make sure that a robot has already passed a pass check BEFORE
+	calling this method
+	*/
+	public function pass($not){//TODO:add some protection for bad calls
+		
+	}
+	
+	//calls robot->tick() methods and does the other things in this decade
+	public function tick(){
+		if($robotA->tick()){
+			$task=$robotA->getTask()
+			if(strcmp($task,"pass")==0){
+				pass();
+			}else(strcmp($task,"shoot")==0){
+				
+			}
+		}
+	}
 }
+//we choose to go to the moon
 ?>
