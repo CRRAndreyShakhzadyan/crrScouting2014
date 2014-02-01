cb0b7b250ad137280df189c0fab1fb541277ca44
crrjamesp <astrophysicsj@gmail.com>
2014-02-01 11:36:42 -0500

    Robot Attributes PHP redux and other fixes
    Silly kids deleted lots of code, generous Daniel is of fix, no worries.

--- php/match_entry.php
+++ php/match_entry.php
@@ -3,7 +3,7 @@
 $match = $team = $barToss = $barCatch = $passes = $recieve = $goalHigh = 
 $goalLow = $defense = $killed = $fouls = $tech_fouls = 0;
 $tags = $notes = $tech_problems = "";
-$high = $low = $hot = $drove = $won = False;
+$high = $low = $noshot = $hot = $drove = $won = False;
 
 //This entire thing just assigns the form data to these variables
 if ($_SERVER["REQUEST_METHOD"] == "POST"){ 
@@ -11,6 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$team = $_POST["team"];
 	$high = (strcmp($_POST["shot_auton"],"high")==0);
 	$low = (strcmp($_POST["shot_auton"],"low")==0);
+	$noshot = (strcmp($_POST["shot_auton"],"slept")==0);
 	$hot = (strcmp($_POST["hot"],"hot")==0);
 	$drove = (strcmp($_POST["drove_auton"],"drove")==0);
 	$barToss = $_POST["bar_toss"];
@@ -23,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$fouls = $_POST["fouls"];
 	$tech_fouls = $_POST["tech_fouls"];
 	$tech_problems = $_POST["tech_problems"];
-	$won = (strcmp($_POST["shot_auton"],"high")==0);
+	$won = isset($_POST['won'];
 	$killed = $_POST["killed"];
 	$notes = $_POST["notes"];
 	$tags = $_POST["tags"];
@@ -36,8 +37,8 @@ if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-mysqli_query($con,"INSERT INTO `match_data`(`match`, `team`, `high_auton`, `low_auton`, `hot_auton`, `drove_auton`,`bar_toss`, `bar_catch`, `passes`, `recieved`,`goal_high`, `goal_low`,`defense`, `fouls`, `tech_fouls`, `tech_problems`, `win/lose`, `notes`, `tags`, `competition`) VALUES 
-(".$match.",".$team.",".$high.",".$low.",".$hot.",".$drove.",".$barToss.",".$barCatch.",".$passes.",".$recive.",".$defense.",`".$fouls.",`".$tech_fouls.",`".$tech_problems.",`".$won.",`".$notes."`,`".$tags."`)");//The good stuff, pass off to mysql
+mysqli_query($con,"INSERT INTO `match_data`(`match`, `team`, `high_auton`, `low_auton`, `no_auton`, `hot_auton`, `drove_auton`,`bar_toss`, `bar_catch`, `passes`, `recieved`,`goal_high`, `goal_low`,`defense`, `fouls`, `tech_fouls`, `tech_problems`, `win/lose`, `notes`, `tags`, `competition`) VALUES 
+(".$match.",".$team.",".$high.",".$low.",".$noshot.",".$hot.",".$drove.",".$barToss.",".$barCatch.",".$passes.",".$recive.",".$defense.",`".$fouls.",`".$tech_fouls.",`".$tech_problems.",`".$won.",`".$notes."`,`".$tags."`)");//The good stuff, pass off to mysql
 
 //Tidy up
 mysqli_close($con);
