5004f4b7838f6a8a05edc758a2c02aff9237bdc4
CRRAndreyShakhzadyan <shakhzaiacc@gmail.com>
2014-02-04 19:02:23 -0500

    BIG CHANGE WOW
    WOW REALLY BIG WOW

--- php/robot_attributes.php
+++ php/robot_attributes.php
@@ -1,24 +1,26 @@
 <?php
 // define variables, set them to default values
 $shoottime = $successrate = 0;
-$drivetype = $drivespeed = $pushiness = $passmethod = $passcomp = $blockmech = "";
+$drivetype = $drivespeed = $pushiness = $passmethod = $passcomp = $blockmech = $blockability = "";
 $pickup = $catcher = $trusscatch = $blockcap = False;
 
 //this assigns form data to the variables
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-	$shoottime = $_POST["shoot_time"];
+	$shoottime = $_POST["shoot"];
 	$successrate = $_POST["goal_rate"];
-	$pickup = isset($_POST['pickup']
-	$catcher = isset($_POST['catcher']
-	$trusscatch = isset($_POST['truss_catch']
-	$blockcap = isset($_POST['block_cap']
+	$pickup = isset($_POST['pickup']);
+	$catcher = isset($_POST['catcher']);
+	$trusscatch = isset($_POST['truss_catch']);
+	$blockcap = isset($_POST['block_cap']);
 	$drivetype = $_POST["drive_type"];
 	$drivespeed = $_POST["drive_speed"];
 	$pushiness = $_POST["drive_push"];
 	$passmethod = $_POST["pass_method"];
 	$passcomp = $_POST["pass_comp"];
 	$blockmech = $_POST["block_mech"];
+	$blockability = $_POST["block_ability"];
+	
 }
 
 $con=mysqli_connect("localhost","root","","scouting_database");
@@ -27,10 +29,14 @@ $con=mysqli_connect("localhost","root","","scouting_database");
 if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+$query = "INSERT INTO `robot_attributes`(`shoot_time`, `goal_rate`, `pickup`, `catcher`, `truss_catch`, `block_cap`, `drive_type`, `drive_speed`, `drive_push`, `pass_method`, `pass_comp`, `block_mech`, `block_ability`) VALUES ('$shoottime','$successrate','$pickup','$catcher','$trusscatch','$blockcap','$drivetype','$drivespeed','$pushiness','$passmethod','$passcomp','$blockmech','$blockability')";
+ 
+echo $query;
 
-mysqli_query($con,"INSERT INTO `robot_attributes`(`shoot_time`, `goal_rate`, `pickup`, `catcher`, `truss_catch`, `block_cap`, `drive_type`,`drive_speed`, `drive_push`, `pass_method`, `pass_comp`,`block_mech`) VALUES 
-(".$shoottime.",".$successrate.",".$pickup.",".$catcher.",".$trusscatch.",".$blockcap.",".$drivetype.",".$drivespeed.",".$pushiness.",".$passmethod.",".$passcomp.",".$blockmech."`)"); //The good stuff, pass off to mysql
-
+if(!mysqli_query($con,$query))
+{
+	die('Error: ' . mysqli_error($con)); 
+}
 //Tidy up
 mysqli_close($con);
 
