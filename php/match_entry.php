98e98d46c89f3a0f4993a519f2684d2ca7148969
samuel-stromswold <samstromsw@gmail.com>
2014-02-07 12:54:06 -0500

    added won
    added fields for winning

--- php/match_entry.php
+++ php/match_entry.php
@@ -8,49 +8,52 @@ $high = $low = $hot = $drove = False;
 if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["match"] != "" && $_POST["team"] != ""){ 
 	$match = $_POST["match"];
 	$team = $_POST["team"];
-	$high = (strcmp($_POST["shot_auton"],"high")==0);
-	$low = (strcmp($_POST["shot_auton"],"low")==0);
-	$hot = (strcmp($_POST["hot"],"hot")==0);
-	$drove = (strcmp($_POST["drove_auton"],"drove")==0);
+	$high = isset($_POST["shot_auton"]);
+	$low = isset($_POST["shot_auton"]);
+	$missed_auton = isset($_POST["missed_auton"]);
+	$hot = isset($_POST["hot"]);
+	$drove = isset($_POST["drove_auton"]);
 	$barToss = $_POST["bar_toss"];
 	$barCatch = $_POST["bar_catch"];
 	$passes = $_POST["passes"];
 	$recive = $_POST["recieved"];
 	$goalHigh = $_POST["goal_high"];
 	$goalLow = $_POST["goal_low"];
+	$missed = $_POST["missed"];
 	$defense = $_POST["defense"];
 	$killed = $_POST["killed"];
+	$won = $_POST["won"];
 	$notes = $_POST["notes"];
 	$tags = $_POST["tags"];
 
-$con=mysqli_connect("localhost","root","","scouting_database");
+	$con=mysqli_connect("localhost","root","","scouting_database");
 
-//Check connection
-if (mysqli_connect_errno()){
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+	//Check connection
+	if (mysqli_connect_errno()){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
 
-$query= 'INSERT INTO `match_data`(`match_number`, `team`, `high_auton`, `low_auton`, `hot_auton`, `drove_auton`,`bar_toss`, `bar_catch`, `passes`, `received`,`goal_high`, `goal_low`,`defense`, `killed`, `notes`, `tags`) VALUES ("'.$match.'","'.$team.'","'.$high.'","'.$low.'","'.$hot.'","'.$drove.'","'.$barToss.'","'.$barCatch.'","'.$passes.'","'.$recive.'","'.$goalHigh.'","'.$goalLow.'","'.$killed.'","'.$defense.'","'.$notes.'","'.$tags.'")';//The good stuff, pass off to mysql
+	$query= 'INSERT INTO `match_data`(`match_number`, `team`, `high_auton`, `low_auton`, `missed_auton`, `hot_auton`, `drove_auton`,`bar_toss`, `bar_catch`, `passes`, `received`,`goal_high`, `goal_low`, `missed`,`defense`, `won`, `killed`, `notes`, `tags`) VALUES ("'.$match.'","'.$team.'","'.$high.'","'.$low.'","'.$missed_auton.'","'.$hot.'","'.$drove.'","'.$barToss.'","'.$barCatch.'","'.$passes.'","'.$recive.'","'.$goalHigh.'","'.$goalLow.'","'.$missed.'","'.$killed.'","'.$won.'","'.$defense.'","'.$notes.'","'.$tags.'")';//The good stuff, pass off to mysql
 
 
-if (!mysqli_query($con,$query))
-  {
-  die('Error: ' . mysqli_error($con));
-  }
+	if (!mysqli_query($con,$query))
+	{
+	die('Error: ' . mysqli_error($con));
+	}
 
-//Tidy up
-mysqli_close($con);
+	//Tidy up
+	mysqli_close($con);
 
-if(strcmp($_POST["tags"],"#econimal") == 0)
-{
-	echo '<iframe width="640" height="360" src="//www.youtube.com/embed/h7yfaSTSxBQ?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>';
-}
-echo("Form submitted successfully. If the back button infuriates you feel free to press <a href='match_entry.html'>here.</a>");
-}
-
-else
-{
+	if(strpos($_POST["tags"],"#econimal") !== false)//I changed it so it just needs to have #econimal anywhere in the string
+	{
+		echo '<iframe width="640" height="360" src="//www.youtube.com/embed/h7yfaSTSxBQ?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>';
+	}
+	
+	echo("Form submitted successfully. If the back button infuriates you feel free to press <a href='match_entry.html'>here.</a>");
+	
+}else{
 	echo "Form was not submitted successfully.  Returning you to the previous page";
+}
 ?>
 
 <html>
@@ -59,7 +62,3 @@ else
 <meta HTTP-EQUIV="REFRESH" content="2; url=match_entry.html">
 
 </html>
-
-<?php
-}
-?>
